4472ddd8685c3e6b4c0f72a71ec133d9de0f50d8
Christian Kirkegaard <hello@christiank.org>
2017-09-12 21:38:32 +0200

    Add example and update readme

---
+++ examples/current.py
@@ -0,0 +1,15 @@
+import tvtid
+
+client = tvtid.Client()
+schedules = client.schedules_for_today()
+channel_length = max(list(map(lambda x: len(x.title), client.channels().values())))
+
+for schedule in schedules:
+    channel = schedule.channel
+    aired, current, upcoming = schedule.current()
+
+    if current:
+        title = channel.title.ljust(channel_length, ' ')
+        print('\n%s [%s] %s ' % (title, current.start_time.strftime('%H:%M'), current.title), end='')
+        for program in upcoming[:3]:
+            print('[%s] %s ' % (program.start_time.strftime('%H:%M'), program.title), end='')
