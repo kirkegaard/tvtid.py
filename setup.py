90d590a4fcedd69a16fd67bd98b657ca8995b740
Christian Kirkegaard <hello@christiank.org>
2017-09-12 17:48:25 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="tvtid",
-    version="0.1.1",
+    version="0.1.2",
     author="Christian Kirkegaard",
     author_email="christian@lowpoly.dk",
     description="Shows you the TV Program",
