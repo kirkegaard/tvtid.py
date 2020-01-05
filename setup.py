c0d2a9a0af6fd78a0cc38f1111eafb71597e228d
Christian Kirkegaard <christian@lowpoly.dk>
2020-01-05 20:25:31 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 import setuptools
 
-try:
-    import pypandoc
-    LONG_DESC = pypandoc.convert("README.md", "rst")
-except(IOError, ImportError, RuntimeError):
-    LONG_DESC = open('README.md').read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="tvtid",
-    version="0.2.1",
+    version="0.2.2",
     author="Christian Kirkegaard",
     author_email="christian@lowpoly.dk",
     description="Library and cli tool for querying tvtid.dk",
-    long_description=LONG_DESC,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/kirkegaard/tvtid.py",
     download_url="https://github.com/kirkegaard/tvtid.py",
     install_requires=[
-        'python-dateutil',
-        'fuzzywuzzy[speedup]',
-        'requests',
-        'requests_cache'
+        "python-dateutil",
+        "fuzzywuzzy[speedup]",
+        "requests",
+        "requests_cache",
     ],
     classifiers=[
         "Environment :: X11 Applications",
@@ -30,11 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
     ],
     py_modules=["tvtid"],
-    entry_points={
-        "console_scripts": [
-            "tvtid=tvtid:main"
-        ]
-    },
+    entry_points={"console_scripts": ["tvtid=tvtid:main"]},
     python_requires=">=3.5",
-    include_package_data=True
+    include_package_data=True,
 )
